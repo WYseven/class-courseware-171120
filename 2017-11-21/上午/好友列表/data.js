5d314ed8157044713845c1ff97c248f2f856c06a
sevent <wangyunhenan168@163.com>
2017-11-21 12:32:22 +0800

    增加2017-11-21上午课件

---
+++ 2017-11-21/上午/好友列表/data.js
@@ -0,0 +1,35 @@
+let data = [
+	{
+		title: '我的好友',
+		list: [
+			{
+				name: 'leo'
+			},
+			{
+				name: 'momo'
+			}
+		]
+	},
+	{
+		title: '企业好友',
+		list: [
+			{
+				name: '奥巴马'
+			},
+			{
+				name: '奥巴马'
+			}
+		]
+	},
+	{
+		title: '黑名单',
+		list: [
+			{
+				name: '杜特尔特'
+			},
+			{
+				name: '杜特尔特'
+			}
+		]
+	}
+]
